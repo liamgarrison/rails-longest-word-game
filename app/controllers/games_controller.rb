2755d6047465971602936cf8205441f64a708e77
Liam Garrison <liamgarrison@gmail.com>
2019-05-09 15:05:29 +0100

    most of score logic complete

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,6 +6,35 @@ class GamesController < ApplicationController
     end
   end
 
+  def letters_to_frequencies(letter_str)
+    frequencies = Hash.new(0)
+    letter_str.split('').each do |letter|
+      frequencies[letter.to_sym] += 1
+    end
+    frequencies
+  end
+
+  def can_be_built?(guessed_letters, given_letters)
+    guessed_letter_freq = letters_to_frequencies(guessed_letters)
+    given_letter_freq = letters_to_frequencies(given_letters)
+
+    guessed_letter_freq.map { |l, freq| freq <= given_letter_freq[l] }.all?
+  end
+
+  def is_english_word?(guessed_letters)
+    true
+  end
+
   def score
+    guessed_letters = params[:guessed_letters].upcase
+    given_letters = params[:given_letters]
+
+    if !is_english_word?(guessed_letters)
+      @result = "Sorry, but #{guessed_letters} is does not seem to be a valid English word..."
+    elsif can_be_built?(guessed_letters, given_letters)
+      @result = "Congratulations! #{guessed_letters} is a valid English word!"
+    else
+      @result = "Sorry but #{guessed_letters} cannot be built out of #{given_letters.split('').join(", ")}"
+    end
   end
 end
